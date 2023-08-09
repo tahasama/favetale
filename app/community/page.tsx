1e78a7b3638d39dc2000c4ec1b386cd2f641e75b
tahasama <taha.maatof@gmail.com>
2023-08-09 23:24:00 +0100

    restyled added explore and community page and divid to smaller components

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,47 +1,70 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const CommunityPage = () => {
+  const [activeTab, setActiveTab] = useState<string>("Pet Images"); // Initial active tab
+
+  const handleTabClick = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+  const tabs = ["Forums", "Meetups", "Events", "Questions"];
+
   return (
-    <section className="py-12 mt-20">
-      <div className="container mx-auhref text-center">
-        <h2 className="text-3xl font-semibold mb-8">Community</h2>
-        <div className="flex flex-col items-center space-y-4">
-          <Link
-            href="/community/forums"
-            className="text-indigo-500 hover:underline"
-          >
-            Forums
-          </Link>
-          <Link
-            href="/community/meetups"
-            className="text-indigo-500 hover:underline"
-          >
-            Meetups
-          </Link>
-          <Link
-            href="/community/user-shrefries"
-            className="text-indigo-500 hover:underline"
-          >
-            User Shrefries
-          </Link>
-          <Link
-            href="/community/events"
-            className="text-indigo-500 hover:underline"
-          >
-            Events
-          </Link>
-          <Link
-            href="/community/questions"
-            className="text-indigo-500 hover:underline"
-          >
-            Questions
-          </Link>
-          {/* Add more links as needed */}
+    <div className="mt-20">
+      <div className="flex justify-center">
+        <div className="flex bg-gray-100 rounded-lg">
+          {tabs.map((tab) => (
+            <TabButton
+              tabName={tab}
+              isActive={activeTab === tab}
+              onClick={() => handleTabClick(tab)}
+            />
+          ))}
         </div>
       </div>
-    </section>
+
+      {/* Content for the active tab */}
+      {activeTab === "Forums" && <Forums />}
+      {activeTab === "Meetups" && <Meetups />}
+      {activeTab === "Events" && <Events />}
+      {activeTab === "Questions" && <Questions />}
+    </div>
   );
+};
+
+const TabButton = ({ tabName, isActive, onClick }: any) => {
+  return (
+    <button
+      className={`px-4 py-2 text-gray-600 ${
+        isActive ? "bg-white border-b-2 border-teal-500" : ""
+      }`}
+      onClick={onClick}
+    >
+      {tabName}
+    </button>
+  );
+};
+
+// Define the content components for each tab
+const Forums = () => {
+  // Render content for the "Pet Images" tab
+  return <div>HHHHHHHHh</div>;
+};
+
+const Meetups = () => {
+  // Render content for the "Advice & Tips" tab
+  return <div>wooooz</div>;
+};
+
+const Events = () => {
+  // Render content for the "Blogs" tab
+  return <div>yaaay</div>;
+};
+
+const Questions = () => {
+  // Render content for the "Inspiring User Stories" tab
+  return <div>hahahaha</div>;
 };
 
 export default CommunityPage;
--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,63 +1,70 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import pet1 from "../images/1.jpg";
-import pet2 from "../images/2.jpg";
-import pet3 from "../images/3.jpg";
+"use client";
+import { useState } from "react";
 
 const ExplorePage = () => {
+  const [activeTab, setActiveTab] = useState<string>("Pet Images"); // Initial active tab
+
+  const handleTabClick = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+  const tabs = ["Gallery", "Tips", "Blogs", "Stories"];
+
   return (
-    <section className="mt-20">
-      <div className="flex flex-col md:flex-row">
-        {/* First Card */}
-        <div className="w-full md:w-1/2 bg-gray-100 p-6 mb-4 md:mr-4 relative">
-          <h2 className="text-2xl font-semibold mb-2">Big Headline</h2>
-          <div className="absolute inset-0">
-            <Image
-              src={pet3}
-              alt="Image 1"
-              className="w-full h-full aspect-auto object-cover scale-75"
+    <div className="mt-20">
+      <div className="flex justify-center">
+        <div className="flex bg-gray-100 rounded-lg">
+          {tabs.map((tab) => (
+            <TabButton
+              tabName={tab}
+              isActive={activeTab === tab}
+              onClick={() => handleTabClick(tab)}
             />
-          </div>
-          <button className="absolute bottom-0 left-0 bg-indigo-500 text-white px-4 py-2 rounded-md shadow-md">
-            Call to Action
-          </button>
-        </div>
-
-        <div className="md:flex md:flex-col md:w-2/5">
-          {/* Second Card */}
-          <div className="bg-gray-100 p-4 mb-4 md:mb-2">
-            <Image
-              src={pet2}
-              alt="Image 2"
-              className="w-full h-auto aspect-auto object-cover mb-4"
-            />
-            <h3 className="text-xl font-semibold mb-2">Headline 2</h3>
-            <p className="text-gray-600 mb-2">Subtext here</p>
-            <button className="bg-indigo-500 text-white px-4 py-2 rounded-md shadow-md">
-              Call to Action
-            </button>
-          </div>
-
-          {/* Third Card */}
-          <div className="flex bg-gray-100 p-6">
-            <div className="flex-grow">
-              <h3 className="text-xl font-semibold mb-2">Headline 3</h3>
-              <p className="text-gray-600 mb-2">Subtext here</p>
-              <button className="bg-indigo-500 text-white px-4 py-2 rounded-md shadow-md">
-                Call to Action
-              </button>
-            </div>
-            <Image
-              src={pet1}
-              alt="Image 3"
-              className="w-1/2 h-auto aspect-auto object-cover"
-            />
-          </div>
+          ))}
         </div>
       </div>
-    </section>
+
+      {/* Content for the active tab */}
+      {activeTab === "Gallery" && <Gallery />}
+      {activeTab === "Tips" && <Tips />}
+      {activeTab === "Blogs" && <Blogs />}
+      {activeTab === "Stories" && <Stories />}
+    </div>
   );
+};
+
+const TabButton = ({ tabName, isActive, onClick }: any) => {
+  return (
+    <button
+      className={`px-4 py-2 text-gray-600 ${
+        isActive ? "bg-white border-b-2 border-teal-500" : ""
+      }`}
+      onClick={onClick}
+    >
+      {tabName}
+    </button>
+  );
+};
+
+// Define the content components for each tab
+const Gallery = () => {
+  // Render content for the "Pet Images" tab
+  return <div>HHHHHHHHh</div>;
+};
+
+const Tips = () => {
+  // Render content for the "Advice & Tips" tab
+  return <div>wooooz</div>;
+};
+
+const Blogs = () => {
+  // Render content for the "Blogs" tab
+  return <div>yaaay</div>;
+};
+
+const Stories = () => {
+  // Render content for the "Inspiring User Stories" tab
+  return <div>hahahaha</div>;
 };
 
 export default ExplorePage;
