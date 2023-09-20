e2372870b7d3d308daecf24901c33694848e5f0b
tahasama <taha.maatof@gmail.com>
2023-09-20 23:37:20 +0100

    general mixed style + quaetion modal

--- app/community/(components)/questions/[id]/page.tsx
+++ app/community/(components)/questions/[id]/page.tsx
@@ -32,7 +32,6 @@ import img11 from "../../../../images/11.jpg";
 import img12 from "../../../../images/12.jpg";
 
 import Image from "next/image";
-import ImageModal from "./QuestionModal";
 import { useParams } from "next/navigation";
 import { motion, useScroll, useTransform } from "framer-motion";
 
