bc15724912127d4b17b894cdb8f3e8999bfa4068
tahasama <taha.maatof@gmail.com>
2023-09-29 15:36:36 +0100

    try 1

--- app/explore/(components)/gallery/page.tsx
+++ app/explore/(components)/gallery/page.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import ServerComponent from "./ServerComponent";
 import Layout from "./layout";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
 
-const Gallery = () => {
+import PetImages from "./PetImages";
+import ImageModal from "./ImageModal";
+
+async function getData() {
+  const petImages: any[] = [];
+  const imageRef = collection(db, "petImages");
+
+  const snapshot = await getDocs(imageRef);
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+  snapshot.forEach((doc: any) => {
+    petImages.push({ id: doc.id, ...doc.data() });
+  });
+  return petImages;
+}
+const Gallery = async () => {
+  const petImages = await getData();
+  console.log(
+    "🚀 ~ file: ServerComponent.tsx:23 ~ ServerComponent ~ petImages:",
+    petImages
+  );
   return (
     <>
       <Layout>
-        {/* <ClientComponent /> */}
-        <ServerComponent />
+        <div className="mt-10  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0 sm:gap-4 mx-2 sm:mx-auto max-w-6xl">
+          {petImages &&
+            petImages.map((image, index) => (
+              <PetImages image={image} index={index} />
+            ))}
+          <ImageModal />
+        </div>
       </Layout>
     </>
   );
