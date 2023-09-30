99a6dfeea615b7bcaf1981883e5b5fad3857df0f
tahasama <taha.maatof@gmail.com>
2023-09-30 16:00:20 +0100

    fixing blog production

--- app/explore/(components)/blogs/page.tsx
+++ app/explore/(components)/blogs/page.tsx
@@ -1,10 +1,4 @@
 import React, { Suspense } from "react";
-import blog1 from "../../../images/blog/blog1.jpg";
-import blog2 from "../../../images/blog/blog2.jpg";
-import blog3 from "../../../images/blog/blog3.jpg";
-import blog4 from "../../../images/blog/blog4.jpg";
-import blog5 from "../../../images/blog/blog5.jpg";
-import blog6 from "../../../images/blog/blog6.jpg";
 
 import ClientComponent from "./ClientComponent";
 import ServerComponent from "./ServerComponent";
