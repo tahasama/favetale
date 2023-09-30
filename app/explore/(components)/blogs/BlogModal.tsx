325a9f360105473619e1d74aab5f6c1f39ab2e4c
tahasama <taha.maatof@gmail.com>
2023-09-30 16:30:08 +0100

    fixing blog production

--- app/explore/(components)/blogs/BlogModal.tsx
+++ app/explore/(components)/blogs/BlogModal.tsx
@@ -1,7 +1,24 @@
 "use client";
 
 const BlogModal = ({ isOpen, onClose }: any) => {
-  return <div>VVVVVVVVVVV</div>;
+  const handleModalClick = (e: any) => {
+    if (e.target.classList.contains("modal-overlay")) {
+      onClose(); // Call the onClose function to close the modal
+    }
+  };
+
+  return (
+    <div
+      className={`linka fixed inset-0 flex flex-col items-center justify-center full w-full mb-4 bg-white  h-screen z-50 backdrop-blur-md backdrop-brightness-50 ${
+        isOpen
+          ? "opacity-100 pointer-events-auto transition-all duration-300"
+          : "opacity-0 pointer-events-none transition-all duration-300"
+      }`}
+      onClick={handleModalClick}
+    >
+      BBBBBBBB
+    </div>
+  );
 };
 
 export default BlogModal;
