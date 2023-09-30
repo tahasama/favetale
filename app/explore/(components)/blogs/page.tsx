f86933b433df28a97737b5264fcb1ff99cb2237d
tahasama <taha.maatof@gmail.com>
2023-09-30 15:44:06 +0100

    fixing blog production

--- app/explore/(components)/blogs/page.tsx
+++ app/explore/(components)/blogs/page.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import blog1 from "../../../images/blog/blog1.jpg";
 import blog2 from "../../../images/blog/blog2.jpg";
 import blog3 from "../../../images/blog/blog3.jpg";
 import blog4 from "../../../images/blog/blog4.jpg";
 import blog5 from "../../../images/blog/blog5.jpg";
 import blog6 from "../../../images/blog/blog6.jpg";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import BlogModal from "./BlogModal";
+
 import ClientComponent from "./ClientComponent";
-import Blog from "./BlogCard";
 import ServerComponent from "./ServerComponent";
 import Loading from "./loading";
 
@@ -58,8 +55,6 @@ const Blogs = () => {
     },
     // Add more blog data here
   ];
-
-  const [uploadModalOpen, setUploadModalOpen] = useState(false);
 
   return (
     <div className="mt-20 w-full">
