9adefc0975e84d0a1c17f0fdbb3fb5142cdf0b19
tahasama <taha.maatof@gmail.com>
2023-09-30 15:56:29 +0100

    fixing blog production

--- app/explore/(components)/blogs/loading.tsx
+++ app/explore/(components)/blogs/loading.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
@@ -8,16 +6,7 @@ export default function Loading() {
   return (
     <>
       {blogs.map((blog: any, index: any) => {
-        <motion.div
-          key={index}
-          initial={{ opacity: 0, x: index * 20 + 50 }} // Initial state (hidden and slightly moved down)
-          animate={{ opacity: 1, x: 0 }} // Animation state (visible and at normal position)
-          transition={{ duration: 0.75, delay: 0.75 }} // Animation duration
-        >
-          <div className="flex flex-col bg-slate-300 shadow-md overflow-hidden  mb-6">
-            <div className="bg-slate-300 h-20 w-20 p-3 smp-6 rounded-lg shadow-md"></div>
-          </div>
-        </motion.div>;
+        <div className="bg-slate-300 h-20 w-20 p-3 smp-6 rounded-lg shadow-md"></div>;
       })}
     </>
   );
--- app/explore/(components)/blogs/page.tsx
+++ app/explore/(components)/blogs/page.tsx
@@ -11,50 +11,6 @@ import ServerComponent from "./ServerComponent";
 import Loading from "./loading";
 
 const Blogs = () => {
-  const blogsData = [
-    {
-      id: 1,
-      title: "Tips for Pet Care",
-      excerpt: "Learn how to provide the best care for your beloved pets...",
-      image: blog1.src,
-    },
-    {
-      id: 2,
-      title: "The Joy of Adopting a Rescue Pet",
-      excerpt: "Discover the heartwarming journey of adopting a rescue pet...",
-      image: blog2.src,
-    },
-    {
-      id: 3,
-      title: "Creating a Pet-Friendly Home Environment",
-      excerpt:
-        "Transform your living space into a safe and welcoming haven for your pets...",
-      image: blog3.src,
-    },
-    {
-      id: 4,
-      title: "Exploring the Benefits of Pet Socialization",
-      excerpt:
-        "Uncover the positive impact of socializing your pets with others...",
-      image: blog4.src,
-    },
-    {
-      id: 5,
-      title: "The Healing Power of Pet Companionship",
-      excerpt:
-        "Discover how pets can bring comfort and healing to our lives...",
-      image: blog5.src,
-    },
-    {
-      id: 6,
-      title: "Preparing for a New Puppy: Tips for First-Time Owners",
-      excerpt:
-        "Get ready to welcome a new puppy into your home with these helpful tips...",
-      image: blog6.src,
-    },
-    // Add more blog data here
-  ];
-
   return (
     <div className="mt-20 w-full">
       <ClientComponent />
