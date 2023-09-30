e4587f9809c64da663ec86192fd6fc37c278bd5b
tahasama <taha.maatof@gmail.com>
2023-09-30 16:27:03 +0100

    fixing blog production

--- app/explore/(components)/blogs/BlogModal.tsx
+++ app/explore/(components)/blogs/BlogModal.tsx
@@ -1,28 +1,7 @@
 "use client";
-import React from "react";
-
-import "react-quill/dist/quill.snow.css"; // Import styles
-import "./blog.css";
 
 const BlogModal = ({ isOpen, onClose }: any) => {
-  const handleModalClick = (e: any) => {
-    if (e.target.classList.contains("modal-overlay")) {
-      onClose(); // Call the onClose function to close the modal
-    }
-  };
-
-  return (
-    <div
-      className={`linka fixed inset-0 flex flex-col items-center justify-center full w-full mb-4 bg-white  h-screen z-50 backdrop-blur-md backdrop-brightness-50 ${
-        isOpen
-          ? "opacity-100 pointer-events-auto transition-all duration-300"
-          : "opacity-0 pointer-events-none transition-all duration-300"
-      }`}
-      onClick={handleModalClick}
-    >
-      BBBBBBBB
-    </div>
-  );
+  return <div>VVVVVVVVVVV</div>;
 };
 
 export default BlogModal;
