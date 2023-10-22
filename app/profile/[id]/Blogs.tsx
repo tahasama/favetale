76f37a6eb8b638eddca9b93ed3d260de083e2416
tahasama <taha.maatof@gmail.com>
2023-10-23 00:29:27 +0100

    profile page re design and fetch initial data

---
+++ app/profile/[id]/Blogs.tsx
@@ -0,0 +1,43 @@
+import { db } from "@/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React from "react";
+
+async function getData(userx: any) {
+  const blogsData: any[] = [];
+  const blogRef = query(
+    collection(db, "blogs"),
+    where("writer.id", "==", userx.id)
+  );
+
+  const snapshot = await getDocs(blogRef);
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+  snapshot.forEach((doc: any) => {
+    blogsData.push({ id: doc.id, ...doc.data() });
+  });
+  return blogsData;
+}
+const Blogs = async ({ tab, userx }: any) => {
+  console.log(
+    "🚀 ~ file: ServerComponent.tsx:23 ~ ServerComponent ~ userx:",
+    userx
+  );
+  const meetupsData = await getData(userx);
+  console.log(
+    "🚀 ~ file: ServerComponent.tsx:25 ~ ServerComponent ~ meetupsData:",
+    meetupsData
+  );
+
+  return (
+    <div>
+      ServerComponent{" "}
+      {meetupsData?.map((meetup: any) => (
+        <p>{meetup.id}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,191 +1,51 @@
 "use client";
-import React, { useState } from "react";
-import user from "../../images/user/userf.jpg";
-import catu from "../../images/user/catu.jpg";
-import catu2 from "../../images/user/catu2.jpg";
-import catu3 from "../../images/user/catu3.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Image from "next/image";
-import { Keyboard, Pagination, Navigation } from "swiper/modules";
-
-import i16 from "../../images/16.jpg";
-import i17 from "../../images/17.jpg";
-import i18 from "../../images/18.jpg";
-import i19 from "../../images/19.jpg";
-
-import blog1 from "../../images/blog/blog1.jpg";
-import blog2 from "../../images/blog/blog2.jpg";
-import blog3 from "../../images/blog/blog3.jpg";
-import blog4 from "../../images/blog/blog4.jpg";
-import blog5 from "../../images/blog/blog5.jpg";
-import blog6 from "../../images/blog/blog6.jpg";
-
-import story1 from "../../images/stories/story1.jpg";
-import story2 from "../../images/stories/story2.jpg";
-import story3 from "../../images/stories/story3.jpg";
-import story4 from "../../images/stories/story4.jpg";
-import story5 from "../../images/stories/story5.jpg";
-import story6 from "../../images/stories/story6.jpg";
-import story7 from "../../images/stories/story7.jpg";
-
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import ImageModal from "../ImageModal";
-import { motion } from "framer-motion";
-import Link from "next/link";
 
-import cage from "../../images/store/cage.jpg";
-import feeder from "../../images/store/feeder.jpg";
-import scratch from "../../images/store/scratch.jpg";
-import { useCart } from "../../provider/CartProvider";
-import { useParams } from "next/navigation";
+import { Suspense } from "react";
+
+import { useCart } from "@/app/provider/CartProvider";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import user from "../../images/user/userf.jpg";
+import { Saira_Semi_Condensed } from "next/font/google";
+import { BsShop } from "react-icons/bs";
+import {
+  FiImage,
+  FiBook,
+  FiClipboard,
+  FiMessageSquare,
+  FiUsers,
+  FiCalendar,
+  FiHelpCircle,
+} from "react-icons/fi";
+import Meetups from "./Meetups";
+import Images from "./Gallery";
+import Gallery from "./Gallery";
+import Blogs from "./Blogs";
+import Stories from "./Stories";
+import Discussions from "./Discussions";
+import Questions from "./Questions";
+
+const font = Saira_Semi_Condensed({ subsets: ["latin"], weight: "400" });
 
 const UserProfile = ({ params: { id } }: any) => {
-  console.log("🚀 ~ file: page.tsx:46 ~ UserProfile ~ params:", id);
-  const catus = [catu, catu2, catu3];
-  const petImages = [i16, i17, i18, i19];
-  const blogsData = [
-    {
-      id: 2,
-      title: "The Joy of Adopting a Rescue Pet",
-      excerpt: "Discover the heartwarming journey of adopting a rescue pet...",
-      image: blog2.src,
-    },
-    {
-      id: 4,
-      title: "Exploring the Benefits of Pet Socialization",
-      excerpt:
-        "Uncover the positive impact of socializing your pets with others...",
-      image: blog4.src,
-    },
-    {
-      id: 5,
-      title: "The Healing Power of Pet Companionship",
-      excerpt:
-        "Discover how pets can bring comfort and healing to our lives...",
-      image: blog5.src,
-    },
-
-    // Add more blog data here
-  ];
-  const blogsData2 = [
-    {
-      id: 1,
-      title: "Tips for Pet Care",
-      excerpt: "Learn how to provide the best care for your beloved pets...",
-      image: blog1.src,
-    },
-
-    {
-      id: 3,
-      title: "Creating a Pet-Friendly Home Environment",
-      excerpt:
-        "Transform your living space into a safe and welcoming haven for your pets...",
-      image: blog3.src,
-    },
-    {
-      id: 6,
-      title: "Preparing for a New Puppy: Tips for First-Time Owners",
-      excerpt:
-        "Get ready to welcome a new puppy into your home with these helpful tips...",
-      image: blog6.src,
-    },
-    // Add more blog data here
-  ];
-  const storiesData = [
-    {
-      id: 1,
-      title: "A Rescue Journey",
-      excerpt:
-        "Read the heartwarming story of how Bella, a shelter dog, found her forever home.Read the heartwarming story of how Bella, a shelter dog, found her forever home.Read the heartwarming story of how Bella, a shelter dog, found her forever home.",
-      image: story1.src,
-    },
-
-    {
-      id: 3,
-      title: "Unbreakable Bond",
-      excerpt:
-        "Explore the extraordinary bond between Sarah and her service dog, Luna, as they conquer life's challenges together.",
-      image: story3.src,
-    },
-    // ... Add more stories
-  ];
-  const storiesData2 = [
-    {
-      id: 2,
-      title: "From Stray to Family",
-      excerpt:
-        "Discover the incredible journey of Max, a stray cat, as he transforms into a beloved family member.",
-      image: story2.src,
-    },
-
-    {
-      id: 10,
-      title: "Pawprints of Love",
-      excerpt:
-        "Join us in celebrating the joyous moments shared by pet owners and their furry companions.",
-      image: story4.src,
-    },
-  ];
-  const products = [
-    {
-      id: 7,
-      name: "Cat Scratching Post",
-      image: scratch.src,
-      price: 24.99,
-      discount: 30,
-      rating: 4.7,
-      reviews: 110,
-      quantity: 1,
-    },
-    {
-      id: 8,
-      name: "Small Animal Cage",
-      image: cage.src,
-      price: 39.99,
-      discount: 25,
-      rating: 4.3,
-      reviews: 70,
-      quantity: 1,
-    },
-    {
-      id: 9,
-      name: "Squirrel Feeder",
-      image: feeder.src,
-      price: 7.99,
-      discount: 15,
-      rating: 4.6,
-      reviews: 90,
-      quantity: 1,
-    },
-  ];
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [image, setImage] = useState();
-  const [expandedPurchase, setExpandedPurchase] = useState<any>(null);
   const { userx } = useCart();
-  console.log("🚀 ~ file: page.tsx:166 ~ UserProfile ~ user:", userx);
+  const [tab, setTab] = useState("Gallery");
 
-  // useEffect(() => {
-  //   first
-
-  //   return () => {
-  //     second
-  //   }
-  // }, [third])
-
-  const openModal = (pet: any) => {
-    setIsModalOpen(true);
-    setImage(pet);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  const tabs = [
+    { name: "Gallery", icon: <FiImage /> },
+    { name: "Blogs", icon: <FiBook /> },
+    { name: "Stories", icon: <FiClipboard /> },
+    { name: "Purchases", icon: <BsShop /> },
+    { name: "Forums", icon: <FiMessageSquare /> },
+    { name: "Meetups", icon: <FiUsers /> },
+    { name: "Events", icon: <FiCalendar /> },
+    { name: "Questions", icon: <FiHelpCircle /> },
+  ];
   return (
-    <div className="bg-teal-50 min-h-screen pt-20">
-      {/* User Information */}
+    <div className="bg-teal-50 min-h-screen h-fit pt-20 pb-10">
       <div className="bg-white mx-6  p-6 mt-8  shadow-md rounded-lg grid grid-cols-1 sm:grid-cols-2  ">
         {/* Profile Picture */}
         <motion.div
@@ -220,6 +80,7 @@ const UserProfile = ({ params: { id } }: any) => {
             </button>
           </div>
         </motion.div>
+
         <motion.div
           initial={{ opacity: 0 }} // Initial state (hidden and slightly moved down)
           animate={{ opacity: 1 }} // Animation state (visible and at normal position)
@@ -227,7 +88,7 @@ const UserProfile = ({ params: { id } }: any) => {
         >
           <div className="grid place-items-center place-content-center">
             <img
-              src={user.src}
+              src={userx.image ? userx.image.src : user.src}
               alt="Profile Picture"
               className="rounded-3xl w-[1/12] object-cover bg-sky-500"
               width={400}
@@ -235,6 +96,34 @@ const UserProfile = ({ params: { id } }: any) => {
           </div>
         </motion.div>
       </div>
+      <div className="bg-white mx-6 p-6 mt-6 shadow-md rounded-lg flex flex-wrap justify-center gap-6">
+        {tabs.map((ta: any, index: any) => (
+          <div
+            onClick={() => setTab(ta.name)}
+            className="relative grid place-items-center w-32 h-32 bg-tealLight ring-1 ring-teal-00 rounded-lg cursor-pointer hover:scale-[1.02] hover:shadow-md transition-all duration-150"
+          >
+            <div className="scale-[2.4] text-blue-900">{ta.icon}</div>
+            <div
+              className={`absolute top-1 text-base ${
+                (font.className,
+                ta.name === tab && "text-blue-600 transition-all duration-300")
+              } `}
+            >
+              {ta.name}
+            </div>
+          </div>
+        ))}
+      </div>
+      <Suspense>
+        {tab === "Gallery" && <Gallery userx={userx} />}
+        {tab === "Blogs" && <Blogs userx={userx} />}
+        {tab === "Stories" && <Stories userx={userx} />}
+        {tab === "Discussions" && <Discussions userx={userx} />}
+        {tab === "Meetups" && <Questions userx={userx} />}
+        {tab === "Meetups" && <Meetups userx={userx} />}
+        {/* {tab === "Meetups" && <Events userx={userx} />}
+        {tab === "Meetups" && <Purchases userx={userx} />} */}
+      </Suspense>
     </div>
   );
 };
