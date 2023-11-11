75c27e048c7df208bdf1f104e4bd5227542e724c
tahasama <taha.maatof@gmail.com>
2023-11-11 13:53:29 +0100

    re fixed DashboardOverview

--- app/dashboard/DashboardOverview/page.tsx
+++ app/dashboard/DashboardOverview/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import ContentDistribution from "./(ContentDistribution)/ContentDistribution";
 import ContentSubmissionTrends from "./(ContentSubmissionTrends)/ContentSubmissionTrends";
 import StoreSalesChart from "./(StoreSalesChart)/StoreSalesChart";
