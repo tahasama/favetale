ffbd94c88e7c0489fcdcaab25129cb8aee39a4bc
tahasama <taha.maatof@gmail.com>
2023-11-29 10:15:05 +0100

    general clean up

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,5 +1,4 @@
-import { m } from "framer-motion";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripeSecret = process.env.STRIPE_SECRET_KEY as string;
@@ -9,7 +8,6 @@ const stripe = new Stripe(stripeSecret, {
 });
 export async function POST(request: Request) {
   const cartDetail = await request.json();
-  console.log("🚀 ~ file: route.ts:12 ~ POST ~ cartDetail:", cartDetail);
   const origin = request.headers.get("origin");
   const session = await stripe.checkout.sessions.create({
     submit_type: "pay",
@@ -22,31 +20,12 @@ export async function POST(request: Request) {
   return NextResponse.json(session);
 }
 
-// export async function GET(request: Request) {
-//   console.log("🚀 ~ file: route.ts:27 ~ GET ~ request:", request);
-//   // const { session_id } = request.json();
-
-//   // try {
-//   //   const session = await stripe.checkout.sessions.retrieve("session_id");
-//   //   return NextResponse.json(session);
-//   // } catch (error) {
-//   //   console.error("Error retrieving session details:", error);
-//   //   return NextResponse.json(
-//   //     {
-//   //       error: "Failed to retrieve session details",
-//   //     },
-//   //     { status: 500 }
-//   //   );
-//   // }
-// }
-
 export async function GET(request: any) {
   const url = request.url;
 
   const urls = new URL(url);
   const sessionId: any = urls.searchParams.get("session_id");
   const session = await stripe.checkout.sessions.retrieve(sessionId);
-  console.log("🚀 ~ file: route.ts:50 ~ GET ~ session:", session);
   return NextResponse.json({
     statusCode: 200,
     body: JSON.stringify(session),
