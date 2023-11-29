4e7edf3873f290ed432ff9c99930bcfd9c4f60f7
tahasama <taha.maatof@gmail.com>
2023-11-29 11:57:46 +0100

    store hero restyling

--- app/store/Hero.tsx
+++ app/store/Hero.tsx
@@ -11,21 +11,13 @@ const Hero = () => {
   return (
     <section
       style={heroStyle}
-      className={`bg-blue-900 text-white mt-20 h-[50vh] flex items-end justify-start w-full `}
+      className={`bg-blue-900 text-white mt-20 h-[50vh] flex items-center justify-center w-full `}
     >
-      <div className="container p-6 w-12/12 lg:w-6/12 h-8/12  backdrop-blur-sm">
-        <h1 className="text-4xl md:xl:text-4xl lg:text-5xl xl:text-6xl md:text-5xl text-emerald-50 font-semibold mb-4 ">
-          Welcome to FaveTale Store
-        </h1>
-        <p className="text-lg mb-8">
-          Your one-stop destination for all your pet needs
-        </p>
-        <Link href={"/store/Allproduct"}>
-          <button className="hover:animate-bounceQ bg-teal-600 hover:bg-teal-600 text-white px-5 py-3 md:px-8 md:py-4 text-lg rounded-md shadow-md transition-all duration-1000 cursor-pointer">
-            Shop Now
-          </button>
-        </Link>
-      </div>
+      <Link href={"/store/Allproduct"}>
+        <button className="hover:animate-bounceQ bg-teal-600 hover:bg-teal-600 text-white px-5 py-3 md:px-7 md:py-3 text-lg rounded-md shadow-md transition-all duration-1000 cursor-pointer">
+          Shop Now
+        </button>
+      </Link>
     </section>
   );
 };
