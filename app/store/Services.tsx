83b9bd5a7f398ac4a1fafe1832535f2086204676
tahasama <taha.maatof@gmail.com>
2023-11-30 23:45:29 +0100

    minor restyling of store product card

--- app/store/Services.tsx
+++ app/store/Services.tsx
@@ -5,7 +5,7 @@ const Services = () => {
   return (
     <section className="pb-16 pt-8 bg-greenLight ">
       <div className="container text-center px-3">
-        <h2 className="text-3xl md:text-4xl font-semibold mb-6">
+        <h2 className="text-2xl md:text-4xl font-semibold mb-6">
           Services and Benefits
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
